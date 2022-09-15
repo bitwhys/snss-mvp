15290f3a6b53bc675f8a205091b1fb48163dfac6
George Bockari <bitwhys@gmail.com>
2022-09-14 23:41:36 -0400

    dev(storybook): setup storybook with chakra and typescript path alias

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+import theme from '../src/theme'
+import '@fontsource/sora/variable-full.css'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  chakra: {
+    theme,
+  },
+}
